03ead34a82133b7ccd8554b844bcb6414ea5508d
kookehs <kookehmonsters@yahoo.com>
2017-07-28 21:14:01 -0700

    complete Generation function

--- genetics.go
+++ genetics.go
@@ -7,49 +7,72 @@ import (
 
 // Genes contains functions necessary for crossover, mutation, and selection.
 type Genes interface {
-	Crossover(g Genes)
-	Fitness(g Genes) float64
-	Mutate()
+	Crossover(genes Genes) (Genes, Genes)
+	Fitness(genes Genes) float64
+	Mutate(chance float64)
 	String() string
 }
 
 // Population contains information necessary for simulation.
 type Population struct {
+	activation float64
 	generation int
 	members    []Genes
+	mutation   float64
 	target     Genes
 }
 
-// NewPopulation returns an empty Population with the given target.
-func NewPopulation(t Genes) *Population {
-	return &Population{generation: 0, target: t, members: []Genes{}}
+// NewPopulation returns an empty Population
+// with the given target and mutation chance.
+func NewPopulation(activation, mutation float64, target Genes) *Population {
+	return &Population{
+		activation: activation,
+		generation: 0,
+		members:    []Genes{},
+		mutation:   mutation,
+		target:     target,
+	}
 }
 
 // Add adds the given Genes to the members
-func (p *Population) Add(g Genes) {
-	p.members = append(p.members, g)
+func (p *Population) Add(genes Genes) {
+	p.members = append(p.members, genes)
+}
+
+// Crossover produces children from the given parents.
+func (p *Population) Crossover(p1, p2 Genes) (Genes, Genes) {
+	return p1.Crossover(p2)
 }
 
 // Display prints the generation and each member.
 func (p *Population) Display() {
 	fmt.Printf("Generation: %d\n", p.generation)
 
-	for _, v := range p.members {
-		fmt.Printf("%g - %s\n", v.Fitness(p.target), v.String())
+	for _, genes := range p.members {
+		fmt.Printf("%g - %s\n", genes.Fitness(p.target), genes.String())
 	}
 }
 
 // Generation simulates a round of selection, crossover, and mutation.
+// Termination is also checked at the end of each round.
 func (p *Population) Generation() bool {
 	p.Sort()
 	p.Display()
 
-	// TODO: Select
-	// TODO: Crossover
-	// TODO: Mutate
+	p1, p2 := p.Selection()
+	c1, c2 := p.Crossover(p1, p2)
 
-	match := false
-	// TODO: Evaluate fitness of genes
+	p.members = p.members[2:]
+	p.members = append(p.members, c1, c2)
+
+	p.generation++
+	match := p.Termination()
+
+	if match {
+		p.Sort()
+		p.Display()
+	}
+
 	return match
 }
 
@@ -60,9 +83,22 @@ func (p *Population) Len() int {
 
 // Less returns if the value at i is less than the value at j.
 func (p *Population) Less(i, j int) bool {
-	m := p.members
-	t := p.target
-	return m[i].Fitness(t) < m[j].Fitness(t)
+	members := p.members
+	target := p.target
+	return members[i].Fitness(target) < members[j].Fitness(target)
+}
+
+// Mutation randomly mutates members of the Population.
+func (p *Population) Mutation() {
+	for _, genes := range p.members {
+		genes.Mutate(p.mutation)
+	}
+}
+
+// Selection returns genes for crossover.
+func (p *Population) Selection() (Genes, Genes) {
+	count := len(p.members)
+	return p.members[count-1], p.members[count-2]
 }
 
 // Sort sorts members of the Population
@@ -72,6 +108,19 @@ func (p *Population) Sort() {
 
 // Swap swaps the value at i with the value at j.
 func (p *Population) Swap(i, j int) {
-	m := p.members
-	m[i], m[j] = m[j], m[i]
+	members := p.members
+	members[i], members[j] = members[j], members[i]
+}
+
+// Termination returns if the target gene has been met.
+func (p *Population) Termination() bool {
+	match := false
+
+	for _, genes := range p.members {
+		if genes.Fitness(p.target) > p.activation {
+			match = true
+		}
+	}
+
+	return match
 }
